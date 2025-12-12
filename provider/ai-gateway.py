10dd3db341c73a6c07d3d81b2445d9f40fca1632
daxingplay <daxingplay@gmail.com>
2025-12-12 08:50:43 +0800

    :construction: llm

--- provider/ai-gateway.py
+++ provider/ai-gateway.py
@@ -2,10 +2,6 @@ import logging
 from collections.abc import Mapping
 
 from dify_plugin import ModelProvider
-from dify_plugin.entities.model import ModelType
-from dify_plugin.errors.model import CredentialsValidateFailedError
-
-logger = logging.getLogger(__name__)
 
 
 class AiGatewayModelProvider(ModelProvider):
@@ -16,12 +12,4 @@ class AiGatewayModelProvider(ModelProvider):
 
         :param credentials: provider credentials, credentials form defined in `provider_credential_schema`.
         """
-        try:
-            pass
-        except CredentialsValidateFailedError as ex:
-            raise ex
-        except Exception as ex:
-            logger.exception(
-                f"{self.get_provider_schema().provider} credentials validate failed"
-            )
-            raise ex
+        pass
